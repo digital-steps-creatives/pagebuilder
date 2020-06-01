beaaa0e6bf2cb6b2f978cf9e251be5c20fbebbe3
Fredrick Boaz <fredrickboaz@gmail.com>
2020-06-01 13:35:41 +0300

    vendor initial

---
+++ src/ServiceProvider.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace HansSchouten\LaravelPageBuilder;
+
+use HansSchouten\LaravelPageBuilder\Commands\CreateTheme;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
+        $this->loadMigrationsFrom(__DIR__ . '/../migrations');
+
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                CreateTheme::class,
+            ]);
+        }
+
+        $this->publishes([
+            __DIR__ . '/../config/pagebuilder.php' => config_path('pagebuilder.php'),
+        ], 'config');
+
+        $this->publishes([
+            __DIR__ . '/../themes/demo' => base_path(config('pagebuilder.theme.folder_url') . '/demo'),
+        ], 'demo-theme');
+    }
+}
